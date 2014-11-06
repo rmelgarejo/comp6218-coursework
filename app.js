71e262dd5bb98778cca31e78c57a328d83c194fd
Christopher Baines <cb15g11@soton.ac.uk>
2014-11-06 12:16:33 +0000

    Add some api routing stuff

--- app.js
+++ app.js
@@ -12,6 +12,52 @@ app.get('/', function (req, res) {
     res.send('Hello World!')
 });
 
+var router = express.Router();
+
+router.get('/questions', function (req, res) {
+  // return a list of quesitons
+  res.json({
+    questions: [
+      {
+        title: "title"
+      }
+    ]
+  });
+});
+
+router.get('/questions/:questionid', function (req, res) {
+  // return a specific question
+  res.json({
+    id: req.params.questionid
+  });
+});
+
+router.get('/questions/:questionid/comments', function (req, res) {
+  // comments for a question
+});
+
+router.get('/questions/:questionid/comments/:commentid', function (req, res) {
+  // specific comment
+});
+
+router.get('/questions/:questionid/answers', function (req, res) {
+  // answers for a question
+});
+
+router.get('/questions/:questionid/answers/:answerid', function (req, res) {
+  // return a specific answer for a specific question
+});
+
+router.get('/questions/:questionid/answers/:answerid/comments', function (req, res) {
+  // comments for an answer
+});
+
+router.get('/questions/:questionid/answers/:answerid/comments/:commentid', function (req, res) {
+  // comment for an answer
+});
+
+app.use('/api', router);
+
 var server = app.listen(3000, function () {
 
     var host = server.address().address
